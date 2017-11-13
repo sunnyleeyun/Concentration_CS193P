59432874bded2b9173754b0feaac5d265923e069
Sunny <sunnylee945@hotmail.com>
2017-11-13 23:22:48 +0800

    add function

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  @IBAction func touchCard(_ sender: UIButton) {
+    flipCard(withEmoji: "👻", on: sender)
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func flipCard(withEmoji emoji: String, on button: UIButton){
+    
   }
 
 
