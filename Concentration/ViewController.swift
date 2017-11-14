c29084836673e85f1949895809f5b7b0227ac122
Sunny <sunnylee945@hotmail.com>
2017-11-14 12:34:17 +0800

    add second card

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -11,10 +11,21 @@ import UIKit
 class ViewController: UIViewController {
 
   @IBAction func touchCard(_ sender: UIButton) {
-    flipCard(withEmoji: "👻", on: sender)
+    flipCard(withEmoji: " 👻", on: sender)
   }
   
+  @IBAction func touchSecondCard(_ sender: UIButton) {
+    flipCard(withEmoji: "🤡", on: sender)
+  }
   func flipCard(withEmoji emoji: String, on button: UIButton){
+    print("flipCard(withEmoji: \(emoji))")
+    if button.currentTitle == emoji{
+      button.setTitle("", for: UIControlState.normal)
+      button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+    } else {
+      button.setTitle(emoji, for: UIControlState.normal)
+      button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
     
   }
 
